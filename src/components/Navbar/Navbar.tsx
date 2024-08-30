79812f61bcb8f388b878abed5d045dcfab73277d
JaredThacker <pricesmayvary302@gmail.com>
2024-08-30 14:27:58 -0400

    refactored navbar, imported lucide-react

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,39 +1,59 @@
-import React from "react"
+'use client';
+
+import { Bell, Menu, Search, Video } from "lucide-react";
+import Link from "next/link";
+import { FormEvent, useRef } from "react"
 
 export const Navbar = () =>{
+    const searchInputRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if(searchInputRef.current){
+            console.log(searchInputRef.current.value)
+        }
+    }
+
     return (
-        <>
-        <div className="navbar bg-base-100">
-        <div className="flex-1">
-            <a className="btn btn-ghost text-xl">REWIND</a>
-        </div>
-        <div className="flex-none gap-2">
-            <div className="form-control">
-            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-            </div>
-            <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                <img
-                    alt="Profile Picture"
-                    src="https://static.vecteezy.com/system/resources/previews/009/292/244/original/default-avatar-icon-of-social-media-user-vector.jpg" />
+        <nav className="fixed top-0 left-0 w-screen z-20 dark:bg-black bg-inherit">
+            <div className='flex justify-between items-center px-2 md:px-7 h-16'>
+                <div className='flex items-center'>
+                <span className='hover:bg-background-dark/30 md:block hidden hover:text-white cursor-pointer rounded-full p-2 mr-3'>
+                    <Menu
+                    onClick={(handleSubmit) => {}}
+                    size={30}
+                    />
+                </span>
+                <Link href='/' className='flex items-center space-x-2'>
+                    <span className='hidden md:block text-2xl font-bold'>REWIND</span>
+                </Link>
+                </div>
+
+                <div className='md:flex items-center justify-center hidden'>
+                <form
+                    onSubmit={handleSubmit}
+                    className='flex items-center h-10 mx-auto'
+                >
+                    <input
+                    type='search'
+                    placeholder='Search'
+                    ref={searchInputRef}
+                    className='px-4 h-full md:w-48 lg:w-96 border dark:border-gray-50 border-gray-300 rounded-l-full focus:outline-none'
+                    />
+                    <div className='h-full px-5 grid place-content-center bg-background-light text-gray rounded-r-full'>
+                    <Search />
+                    </div>
+                </form>
+                </div>
+                <div className="flex items-center space-x-7">
+                    <div className="md:hidden">
+                        {/* theme toggler */}
+                    </div>
+                    <Video/>
+                    <Bell/>
                 </div>
             </div>
-            <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                <li>
-                <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                </a>
-                </li>
-                <li><a>Settings</a></li>
-                <li><a>Logout</a></li>
-            </ul>
-            </div>
-        </div>
-        </div>
-        </>
+        </nav>
     )
 };
