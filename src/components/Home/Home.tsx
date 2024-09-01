9ec0512b23ac60a889173c93a7ca52228b05c062
JaredThacker <pricesmayvary302@gmail.com>
2024-09-01 15:22:41 -0400

    cant figure out why thumbnail div isnt showing up

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import useSWR from "swr";
 import { fetchVideos } from "@/lib/api";
+import Thumbnail from "../Thumbnail/Thumbnail";
 
 export const Home = () => {
   const [badge, setBadge] = useState("all");
@@ -12,5 +13,17 @@ export const Home = () => {
     isLoading,
   } = useSWR(`fetchVideo/${badge}`, () => fetchVideos("badge", 9));
 
-  return <></>;
+  return (
+    <>
+      <div className="px-2 md:pl-[252px] fixed top-16 py-2 left-0 w-screen z-20 dark:bg-black bg-white">
+        Search Badge
+      </div>
+
+      <div className="flex flex-wrap">
+        {videoResults?.map((video) => (
+          <Thumbnail key={video.id} video={video} />
+        ))}
+      </div>
+    </>
+  );
 };
