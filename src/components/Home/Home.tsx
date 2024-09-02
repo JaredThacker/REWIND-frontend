9d64aa5e1dd0fc88741c51fe306509181deef448
JaredThacker <pricesmayvary302@gmail.com>
2024-09-01 22:10:00 -0400

    rollback

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,30 +2,14 @@
 
 import React, { useState } from "react";
 import useSWR from "swr";
-import { fetchVideos } from "@/lib/api";
-import Thumbnail from "../Thumbnail/Thumbnail";
+import { Navbar } from "../Navbar/Navbar";
+import MainFeed from "../MainFeed/mainFeed";
 
 export const Home = () => {
-  const [badge, setBadge] = useState("all");
-  const {
-    data: videoResults,
-    error,
-    isLoading,
-  } = useSWR(`fetchVideo/${badge}`, () => fetchVideos(badge, 9));
-
-  console.log(videoResults);
-
   return (
     <>
-      <div className="px-2 md:pl-[252px] fixed top-16 py-2 left-0 w-screen z-20 dark:bg-black bg-white">
-        Search Badge
-      </div>
-
-      <div className="flex flex-wrap">
-        {videoResults?.map((video) => (
-          <Thumbnail key={video.id} video={video} />
-        ))}
-      </div>
+      <Navbar />
+      <MainFeed />
     </>
   );
 };
