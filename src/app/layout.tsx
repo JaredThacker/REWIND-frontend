7672a38a8b7d9256d1b9523614ebab7ce356654a
JaredThacker <pricesmayvary302@gmail.com>
2024-09-03 11:05:47 -0400

    cleaned up a bit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Poiret_One, Limelight, Quicksand } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/Theme/theme-provider";
 import { Navbar } from "@/components/Navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
--- src/components/MainFeed/mainFeed.tsx
+++ src/components/MainFeed/mainFeed.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { API_KEY } from '../../data';
-import { VideoItem, VideoListResponse } from './YoutubeAPIFeed';
-import './mainFeed.css';
-import { truncateByWords, formatViews} from './truncateViewAndTitle'
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { API_KEY } from "../../data";
+import { VideoItem, VideoListResponse } from "./YoutubeAPIFeed";
+import "./mainFeed.css";
+import { truncateByWords, formatViews } from "./truncateViewAndTitle";
 import Image from "next/image";
 
 const MainFeed: React.FC = () => {
@@ -19,7 +19,7 @@ const MainFeed: React.FC = () => {
       setError(null);
 
       try {
-        const videoListUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=US&key=${API_KEY}`;
+        const videoListUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${API_KEY}`;
         const response = await fetch(videoListUrl);
         const result: VideoListResponse = await response.json();
         setData(result.items);
@@ -41,9 +41,14 @@ const MainFeed: React.FC = () => {
       {data.map((item) => (
         <Link href={`/video/${item.id}`} key={item.id}>
           <div className="card">
-            <img src={item.snippet.thumbnails.medium.url} alt={item.snippet.title} />
+            <img
+              src={item.snippet.thumbnails.medium.url}
+              alt={item.snippet.title}
+            />
             <h2>{truncateByWords(item.snippet.title, 8)}</h2>
-            <h4>{formatViews(parseInt(item.statistics.viewCount, 10))} views</h4>
+            <h4>
+              {formatViews(parseInt(item.statistics.viewCount, 10))} views
+            </h4>
             <h3>{item.snippet.channelTitle}</h3>
           </div>
         </Link>
