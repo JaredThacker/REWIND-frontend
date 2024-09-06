941d260dd7fc38a75cbd94870001dfa50a857acb
JaredThacker <pricesmayvary302@gmail.com>
2024-09-06 15:41:48 -0400

    upload working

--- src/components/Upload/Upload.tsx
+++ src/components/Upload/Upload.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import UploadVideoToS3 from "./UploadVideoToS3";
+import UploadVideoToS3WithNativeSdk from "./UploadVideoToS3NativeSDK";
 
 export const Upload = () => {
   return (
     <div className="h-full w-full">
       <div className="pt-64 flex flex-col align-middle items-center">
-        <UploadVideoToS3 />
+        <UploadVideoToS3WithNativeSdk />
       </div>
     </div>
   );
