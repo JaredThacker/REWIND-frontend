ae664ce3c33b2937201074207b4a5ead28a9a9c3
JaredThacker <pricesmayvary302@gmail.com>
2024-09-06 11:07:24 -0400

    started upload page

---
+++ src/components/Upload/Upload.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Upload = () => {
+  return (
+    <div className="h-full w-full">
+      <div className="pt-64 flex flex-col align-middle items-center">
+        <input type="file" className="animate-fadeIn items-center"></input>
+      </div>
+    </div>
+  );
+};
